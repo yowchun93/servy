d33ddd8c9839fa1c882ae4f6c5a1c42a596f95b9
yc yap <yowchun93@gmail.com>
2019-11-23 16:20:16 +0800

    Add Server Routers

---
+++ lib/servy/handler.ex
@@ -0,0 +1,74 @@
+defmodule Servy.Handler do
+
+  # request = """
+  # GET /wildthings HTTP/1.1
+  # Host: example.com
+  # User-Agent: ExampleBrowser/1.0
+  # Accept: */*
+  # """
+  def handle(request) do
+    request |> parse |> route |> format_response
+  end
+
+  def log(conv) do
+    IO.inspect conv
+  end
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split(" ")
+    %{ method: method, path: path, resp_body: "", status: nil}
+  end
+
+  def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington", status: 200}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | resp_body: "No #{path} here!", status: 404}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
+end
+
+
+# response = Servy.Handler.handle(request)
